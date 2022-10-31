40a0469331ce4225603dd1a0a57a106edfe90f4d
Noahkimtai <noahkimtai24@gmail.com>
2022-11-01 01:00:03 +0300

    refactored

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,11 +7,11 @@ class EmailAddressParser
     attr_accessor :emails
 
     def initialize(emails)
-        @emails = emails.gsub(",", "").split(" ")
+        @emails = emails
     end
 
     def parse
-        emails.uniq
+        emails.gsub(",", "").split(" ").uniq
     
     end
 end
